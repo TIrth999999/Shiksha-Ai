33759de748b494629008c0770d3f0034b8f65acc
Tirth999999 <tirthgajera12345@gmail.com>
2025-07-18 13:52:11 +0530

    Added

--- ShikshaAi/settings.py
+++ ShikshaAi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7258zc4kbrqzgda-1iv%#%hxb6nia$nfp@k58cza1ai4ypy9!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
